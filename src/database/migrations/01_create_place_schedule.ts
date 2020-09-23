a5c7c6fb42adb6cf36e0609a263ecca4a57ad0bf
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-22 22:49:20 -0300

    feat: add migrations

---
+++ src/database/migrations/01_create_place_schedule.ts
@@ -0,0 +1,22 @@
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('place_schedule', table => {
+        table.increments('id').primary()
+
+        table.integer('week_day').notNullable()
+        table.integer('from').notNullable()
+        table.integer('to').notNullable()
+
+        table.integer('place_id')
+            .notNullable()
+            .references('id')
+            .inTable('places')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('place_schedule')
+} 
