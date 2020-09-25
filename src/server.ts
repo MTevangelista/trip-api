c03ab49744c09fa869efa2a2da68b2d2bdce0e93
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-09-25 11:51:23 -0300

    refactor: configure heroku port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = 3333;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
